78ed1a438dbba8f5afd6d2386abd265802fdd645
chaminnk <chaminnk.16@cse.mrt.ac.lk>
2020-06-08 19:42:06 +0530

    Drop nan pickup-drop seconds long lat dist f1=0.97814

--- fare_classification.py
+++ fare_classification.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from datetime import datetime
 
 from sklearn.model_selection import train_test_split
@@ -12,7 +13,8 @@ from sklearn.model_selection import cross_val_score
 from sklearn.metrics import classification_report
 from sklearn.metrics import f1_score
 from xgboost import XGBClassifier
-from catboost import CatBoostClassifier
+from xgboost import plot_importance
+from catboost import CatBoostClassifier, Pool
 import lightgbm as lgb
 from sklearn.linear_model import LogisticRegression
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
@@ -43,40 +45,102 @@ def timer(start_time=None):
 
 input_file = "train.csv"
 df = pd.read_csv(input_file)
-df.fillna(df.mean(), inplace=True)
-#df.fillna(0, inplace=True)
+##print(df.isnull().sum())
+##df.dropna(inplace= True)
+##df.reset_index(drop=True,inplace= True)
+##df = df.dropna(how = 'any', axis = 'rows')
 X = df
+##print(X.dtypes)
 ##X = df.drop(columns=["tripid", "pickup_time","drop_time","label"])
+X["pickup_time"] = X["pickup_time"].astype('datetime64[m]')
+##X["pickup_year"] = X["pickup_time"].dt.year.astype('float')
+##X["pickup_month"] = X["pickup_time"].dt.month.astype('float')
+##X["pickup_day"] = X["pickup_time"].dt.day.astype('float')
+##X["pickup_week"] = X["pickup_time"].dt.week.astype('float')
+##X["pickup_hour"] = X["pickup_time"].dt.hour.astype('float')
+##X["pickup_minute"] = X["pickup_time"].dt.minute.astype('float')
+##X["pickup_day_of_week"] = X["pickup_time"].dt.dayofweek.astype('float')
+##
+X["drop_time"] = X["drop_time"].astype('datetime64[m]')
+##X["drop_year"] = X["drop_time"].dt.year.astype('float')
+##X["drop_month"] = X["drop_time"].dt.month.astype('float')
+##X["drop_day"] = X["drop_time"].dt.day.astype('float')
+##X["drop_week"] = X["drop_time"].dt.week.astype('float')
+##X["drop_hour"] = X["drop_time"].dt.hour.astype('float')
+##X["drop_minute"] = X["drop_time"].dt.minute.astype('float')
+##X["drop_day_of_week"] = X["drop_time"].dt.dayofweek.astype('float')
+
+##X["pickup_time"]= X["pickup_time"].dt.time
+##X["drop_time"]= X["drop_time"].dt.time
+##X["pickup_time"] =  X["pickup_time"].astype('timedelta64[s]')
+##X["drop_time"] = X["drop_time"].astype('timedelta64[s]')
+##with pd.option_context('display.max_columns', None):  
+##    print(X)
+
+X["trip_duration"] = X["drop_time"]-X["pickup_time"]
+X["trip_duration"] = X["trip_duration"].dt.total_seconds() #very less important feature
+##X['ride_fare'] = X['fare']-X['meter_waiting_fare']-X['additional_fare']
+##X['fare_duration_ratio'] = X['fare']/X["duration"]
 X['distance'] = Distance(X['pick_lat'].tolist(),X['pick_lon'].tolist(),X['drop_lat'].tolist(),X['drop_lon'].tolist())
 X = df.drop(columns=["tripid","pickup_time","drop_time","pick_lat","pick_lon","drop_lat","drop_lon","label"])
+
 #X = df.drop(columns=["tripid","label"])
+##with pd.option_context('display.max_columns', None):  
+##    print(X)
+##print(X.dtypes)
+##
 with pd.option_context('display.max_columns', None):  
     print(X)
 X = X.iloc[:,:].values
-print(X)
+##print(X)
 
 df['output_label'] = (df['label'] == 'correct').astype('int')
 y = df["output_label"].values
 
 
-
 input_file2 = "test.csv"
 df2 = pd.read_csv(input_file2)
-df2.fillna(df2.mean(), inplace=True)
-#df2.fillna(0, inplace=True)
+##df2.dropna(inplace= True)
+##df2.reset_index(drop=True,inplace= True)
 tripid_test = np.asarray(df2.iloc[:, 0].values)
 X2 = df2
-##X2 = df2.drop(columns=["tripid","pickup_time","drop_time"])
+X2["pickup_time"] = X2["pickup_time"].astype('datetime64[m]')
+##X2["pickup_year"] = X2["pickup_time"].dt.year.astype('float')
+##X2["pickup_month"] = X2["pickup_time"].dt.month.astype('float')
+##X2["pickup_day"] = X2["pickup_time"].dt.day.astype('float')
+##X2["pickup_week"] = X2["pickup_time"].dt.week.astype('float')
+##X2["pickup_hour"] = X2["pickup_time"].dt.hour.astype('float')
+##X2["pickup_minute"] = X2["pickup_time"].dt.minute.astype('float')
+##X2["pickup_day_of_week"] = X2["pickup_time"].dt.dayofweek.astype('float')
+##
+X2["drop_time"] = X2["drop_time"].astype('datetime64[m]')
+##X2["drop_year"] = X2["drop_time"].dt.year.astype('float')
+##X2["drop_month"] = X2["drop_time"].dt.month.astype('float')
+##X2["drop_day"] = X2["drop_time"].dt.day.astype('float')
+##X2["drop_week"] = X2["drop_time"].dt.week.astype('float')
+##X2["drop_hour"] = X2["drop_time"].dt.hour.astype('float')
+##X2["drop_minute"] = X2["drop_time"].dt.minute.astype('float')
+##X2["drop_day_of_week"] = X2["drop_time"].dt.dayofweek.astype('float')
+####with pd.option_context('display.max_columns', None):  
+####    print(X2)
+##print(X2.dtypes)
+X2["trip_duration"] = X2["drop_time"]-X2["pickup_time"]
+X2["trip_duration"] = X2["trip_duration"].dt.total_seconds()
+##X2['fare_duration_ratio'] = X2['fare']/X2["duration"]
 X2['distance'] = Distance(X2['pick_lat'].tolist(),X2['pick_lon'].tolist(),X2['drop_lat'].tolist(),X2['drop_lon'].tolist())
 X2 = df2.drop(columns=["tripid","pickup_time","drop_time","pick_lat","pick_lon","drop_lat","drop_lon"])
 #X2 = df2.drop(columns=["tripid"])
-with pd.option_context('display.max_columns', None):  
-    print(X2)
+##with pd.option_context('display.max_columns', None):  
+##    print(X2)
+##print(X2.dtypes)
+
 X2 = X2.iloc[:,:].values
-print(X2)
+##print(X2)
 #print (df.iloc[4080:4085])
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
-#print(X_train)
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+##print(len(X))
+##print(X_train)
+##print(X_test)
 lr = 0.404
 def knnClassifier(X_train,X_test,y_train,y_test):
     print("knn")
@@ -121,38 +185,38 @@ def adaboostClassifier(X_train,X_test,y_train,y_test):
 def xgboostModel(X_train,X_test,y_train,y_test,tripid_test):
     print("xgboost")
     
-##    n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
+    n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
 ##    learning_rate = []
-##    n = 0.402
+##    n = 0.403
 ##    for i in range (0,51):
 ##        n = n + 0.0001
 ##        n = round(n,4)
 ##        learning_rate.append(n)
-##    params = {
-##        'n_estimators':n_estimators,
-##        'learning_rate':learning_rate,
-##        'min_child_weight': [1, 5, 10],
-##        'gamma': [8, 9, 10, 12, 15],
-##        'subsample': [0.7,0.8, 0.9, 1.0,1.1],
-##        'colsample_bytree': [0.7,0.8, 0.9, 1.0,1.1],
-##        'max_depth': [4, 5, 7,9,11]
-##        }
+    params = {
+        'n_estimators':n_estimators,
+        'learning_rate':[0.4038,0.4039,0.404,0.4041,0.4042],
+        'min_child_weight': [1, 5, 10],
+        'gamma': [8, 9, 10, 12, 15],
+        'subsample': [0.7,0.8, 0.9, 1.0,1.1],
+        'colsample_bytree': [0.7,0.8, 0.9, 1.0,1.1],
+        'max_depth': [4, 5, 7,9,11]
+        }
 ##    param_comb = 50
     
 ##    model4 = XGBClassifier(learning_rate=0.404, n_estimators=600, objective='binary:logistic',
 ##                    silent=True, nthread=1)
     model4 = XGBClassifier(silent=False,
                                scale_pos_weight=1,
-                               learning_rate=0.404,
+                               learning_rate=0.405,
                                colsample_bytree = 0.9,
                                subsample = 0.9,
                                objective='binary:logistic',
-                               n_estimators=1110,
+                               n_estimators=1200,
                                reg_alpha = 0.3,
                                max_depth=7,
-                               gamma=10
+                               gamma=10,
                                )
-    
+##    
 ##    model4 = XGBClassifier(base_score=0.5, booster=None, colsample_bylevel=1,
 ##              colsample_bynode=1, colsample_bytree=1.0, gamma=10, gpu_id=-1,
 ##              importance_type='gain', interaction_constraints=None,
@@ -163,7 +227,7 @@ def xgboostModel(X_train,X_test,y_train,y_test,tripid_test):
 ##              reg_lambda=1, scale_pos_weight=1, silent=False, subsample=1.0,
 ##              tree_method=None, validate_parameters=False, verbosity=None)
 
-##    model4 = XGBClassifier()
+    model4 = XGBClassifier(n_estimators=90)
 ##    random_search = RandomizedSearchCV(model4, param_distributions=params, n_iter=100, cv = 3, scoring='roc_auc', n_jobs=-1, verbose=2, random_state=42 )
 ##    start_time = timer(None) # timing starts from this point for "start_time" variable
 ##    random_search.fit(X_train, y_train)
@@ -183,27 +247,37 @@ def xgboostModel(X_train,X_test,y_train,y_test,tripid_test):
 ##        results.to_csv(f, float_format='%.2f', index=False)
 
     model4.fit(X_train, y_train)
-    y_pred=model4.predict(X_test)
+
+    #k-fold cross validation
+##    y_pred=model4.predict(X_test)
 ##    print(f1_score(y_test,y_pred))
-    
+
+    #create output csv file
     tripid_test.resize(8576, 1)
     data = np.column_stack([tripid_test, y_pred])
-    print(tripid_test)
     label = ["tripid", "prediction"]
     frame = pd.DataFrame(data, columns=label)
     file_path = "./xgb_output.csv"
     with open(file_path, mode='w', newline='\n') as f:
         frame.to_csv(f, float_format='%.2f', index=False, header=True)
-        
+
+    # plot feature importance
+    plot_importance(model4)
+    pyplot.show()
     # Look at parameters used by our current classifier
     print('Parameters currently in use:\n')
     print(model4.get_params())
 
 def catBoost(X_train,X_test,y_train,y_test,tripid_test):
     print("Catboost")
+####    eval_pool = Pool(X_test, y_test)
     ##categorical_features_indices = np.where(X_train.dtypes != np.float)[0]
-    model5 = CatBoostClassifier(iterations=55, depth=3, learning_rate=0.407)
+##    model5 = CatBoostClassifier(iterations=310, depth=3, learning_rate=0.408)
+    model5 = CatBoostClassifier( iterations = 152,verbose = 100)
+
+##    model5.fit(X_train, y_train, eval_set=eval_pool, early_stopping_rounds=10)
     model5.fit(X_train, y_train)
+
     y_pred=model5.predict(X_test)
     print(f1_score(y_test,y_pred))
     
@@ -273,8 +347,8 @@ xgboostModel(X,X2,y,y_test,tripid_test)
 ##xgboostModel(X_train,X_test,y_train,y_test,tripid_test)
 ##randomForestModel(X_train,X_test,y_train,y_test,tripid_test)
 ##randomForestModel(X,X2,y,y_test,tripid_test)
-#catBoost(X_train,X_test,y_train,y_test,tripid_test)
-#catBoost(X,X2,y,y_test,tripid_test)
+##catBoost(X_train,X_test,y_train,y_test,tripid_test)
+##catBoost(X,X2,y,y_test,tripid_test)
     
 ##model = DecisionTreeClassifier()
 ##model.fit(X_train, y_train)
@@ -307,7 +381,7 @@ xgboostModel(X,X2,y,y_test,tripid_test)
 ####	level0.append(('cart', DecisionTreeClassifier()))
 ####	level0.append(('svm', SVC()))
 ####	level0.append(('bayes', GaussianNB()))
-##	level0.append(('rf', RandomForestClassifier()))
+####	level0.append(('rf', RandomForestClassifier()))
 ##	level0.append(('xgboost',XGBClassifier(silent=False,
 ##                               scale_pos_weight=1,
 ##                               learning_rate=0.404,
@@ -317,10 +391,9 @@ xgboostModel(X,X2,y,y_test,tripid_test)
 ##                               n_estimators=1110,
 ##                               reg_alpha = 0.3,
 ##                               max_depth=7,
-##                               gamma=10,
-##                               reg_lambda=1
+##                               gamma=10
 ##                               )))
-####	level0.append(('catboost', CatBoostClassifier(iterations=55, depth=3, learning_rate=0.407)))
+##	level0.append(('catboost', CatBoostClassifier(iterations=55, depth=3, learning_rate=0.407)))
 ##	
 ##	# define meta learner model
 ##	level1 = LogisticRegression()
@@ -336,7 +409,7 @@ xgboostModel(X,X2,y,y_test,tripid_test)
 ####	models['cart'] = DecisionTreeClassifier()
 ####	models['svm'] = SVC()
 ####	models['bayes'] = GaussianNB()
-##	models['rf'] = RandomForestClassifier()
+####	models['rf'] = RandomForestClassifier()
 ##	models['xgboost'] = XGBClassifier(silent=False,
 ##                               scale_pos_weight=1,
 ##                               learning_rate=0.404,
@@ -348,7 +421,7 @@ xgboostModel(X,X2,y,y_test,tripid_test)
 ##                               max_depth=7,
 ##                               gamma=10
 ##                               )
-####	models['catboost'] = CatBoostClassifier(iterations=55, depth=3, learning_rate=0.407)
+##	models['catboost'] = CatBoostClassifier(iterations=55, depth=3, learning_rate=0.407)
 ##	models['stacking'] = get_stacking()
 ##	return models
 ## 
@@ -371,3 +444,4 @@ xgboostModel(X,X2,y,y_test,tripid_test)
 ### plot model performance for comparison
 ##pyplot.boxplot(results, labels=names, showmeans=True)
 ##pyplot.show()
+
